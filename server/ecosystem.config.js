21cf0c2908fa04276919a8ad64df50b45f3144ea
Winston Malcolm <winston.malcolm2016@gmail.com>
2026-05-31 20:26:37 -0500

    Add server and website ecosystem configuration files; update .gitignore and package.json dependencies

---
+++ server/ecosystem.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  apps: [
+    {
+      name:        'writium-server',
+      script:      'dist/index.js',
+      instances:   1,
+      exec_mode:   'fork',
+      watch:       false,
+      restart_delay: 4000,
+      max_restarts:  10,
+
+      env_production: {
+        NODE_ENV: 'production',
+        PORT:     3001,
+      },
+
+      error_file:      'logs/pm2-error.log',
+      out_file:        'logs/pm2-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+    },
+  ],
+}
